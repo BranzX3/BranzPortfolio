9764729405580eaab564474e81edb168387487b3
BranzX3 <bunzzbranzz@gmail.com>
2026-05-07 12:49:51 +0700

    Revert "ฟ"
    This reverts commit a113ebfb36f8ff15a0f397612e3dd17c2b2a110b.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,18 +43,6 @@ export default function Navbar() {
               </li>
             ))}
             <li>
-              <button 
-                onClick={() => {
-                  window.print();
-                  handleNavClick();
-                }}
-                className="no-print"
-                aria-label="Export to PDF"
-              >
-                📄 PDF
-              </button>
-            </li>
-            <li>
               <a href="#contact" className="nav-cta" onClick={handleNavClick}>
                 Hire Me
               </a>
